1994e701e8bb5e96fd394e32b2ad0532f7e11bcb
Faisal Amiruddin <159210623+faisal-amiruddin@users.noreply.github.com>
2025-01-13 16:33:45 +0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,7 +17,7 @@ export const About = () => {
                     <img src="pp.jpg" alt="Me" />
                 </div>
                 <div className='about-content'>
-                    <h1 data-aos="fade-up" data-aos-duration="2000">About Me1</h1>
+                    <h1 data-aos="fade-up" data-aos-duration="2000">About Me</h1>
                     <p data-aos="fade-up" data-aos-delay="500" data-aos-duration="2500">I am an Informatics Engineering student with a strong passion for web development, continuously learning and improving my skills in the field. Although I am just starting out as a frontend developer, I focus on creating responsive websites and interactive applications that deliver optimal user experiences.</p>
                     <p data-aos="fade-up" data-aos-delay="600" data-aos-duration="2500">I always strive to combine creative design with efficient code to develop solutions that meet user needs.</p>
                 </div>
