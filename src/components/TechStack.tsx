715b99ef895e4fdd41b250e2b4397b2a0502d117
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-01-15 22:58:35 +0700

    add show more tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,6 +1,37 @@
+import { useState } from 'react'
 import './TechStack.css'
 
 export const TechStack = () => {
+    const stacksLess = [
+        {icon: 'devicon-html5-plain', name: 'HTMl'},
+        {icon: 'devicon-css3-plain', name: 'CSS'},
+        {icon: 'devicon-javascript-plain', name: 'JavaScript'},
+        {icon: 'devicon-typescript-plain', name: 'TypeScript'},
+        {icon: 'devicon-react-original', name: 'React'},
+    ]
+
+    const stacksAll = [
+        {icon: 'devicon-html5-plain', name: 'HTMl'},
+        {icon: 'devicon-css3-plain', name: 'CSS'},
+        {icon: 'devicon-javascript-plain', name: 'JavaScript'},
+        {icon: 'devicon-typescript-plain', name: 'TypeScript'},
+        {icon: 'devicon-react-original', name: 'React'},
+        {icon: 'devicon-mysql-original', name: 'MySQL'},
+        {icon: 'devicon-java-plain', name: 'Java'},
+    ]
+
+    const [isShow, setIsShow] = useState(false);
+
+    const handleShow = () => {
+        if(!isShow) {
+            setIsShow(true)
+        } else {
+            setIsShow(false)
+        }
+    }
+
+    const currentShow = isShow ? stacksAll : stacksLess;
+
     return (
         <>
             <div className='tech-scroll'></div>
@@ -9,34 +40,20 @@ export const TechStack = () => {
                     <h1 data-aos="fade" data-aos-duration="2000">Tech Stacks</h1>
                 </div>
                 <div className="skills">
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-html5-plain"></i>
-                        <h3>HTML5</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-css3-plain"></i>
-                        <h3>CSS3</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-javascript-plain"></i>
-                        <h3>JavaScript</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-typescript-plain"></i>
-                        <h3>TypeScript</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-react-original"></i>
-                        <h3>React</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-mysql-original"></i>
-                        <h3>MySQL</h3>
-                    </div>
-                    <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
-                        <i className="devicon-java-plain"></i>
-                        <h3>Java</h3>
-                    </div>
+                    {
+                        currentShow.map((stack) => (
+                            <div className="skill-item" data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
+                                <i className={stack.icon}></i>
+                                <h3>{stack.name}</h3>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className='load' data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000" onClick={handleShow}>
+                    {
+                        isShow? (<p>Show Less</p>): (<p>Load More</p>)
+                    }
+                    <i className='load-indicator fas fa-chevron-down'></i>
                 </div>
             </section>
         </>
